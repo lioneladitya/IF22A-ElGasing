bcaed8c9381dadf2518774b8189e50e28ac5f356
Lionel Aditya <adityaindomaret@gmail.com>
2025-01-14 15:23:36 +0700

    Surya Aditya : Menambahkan Destinasi Urlnya

---
+++ app/constants/destinationUrls.ts
@@ -0,0 +1,8 @@
+export const destinationUrls = {
+    'The Great Rift Valley': 'https://maps.app.goo.gl/U23nq2Xu6tPy6Lg77',
+    'Giraffe Centre Nairobi': 'https://maps.app.goo.gl/xpoRnZmyq8wkWJLf6',
+    'Menengai Crater View Point': 'https://maps.app.goo.gl/2eonfdnnNQF3MdeKA',
+    'Mount Kenya': 'https://maps.app.goo.gl/yTeZfK8zCZJvbso78',
+    'Pyramids of Giza': 'https://maps.app.goo.gl/VGoMEW1Upq9YTyJJA',
+  };
+  
