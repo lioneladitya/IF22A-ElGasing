51d7cde8cd37dda7499ab1f4849ba473607ba366
Lionel Aditya <adityaindomaret@gmail.com>
2025-01-17 22:57:24 +0700

    Surya Aditya : Mengganti Tampilan Text & Mengganti foto

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -12,11 +12,11 @@ const Hero = () => {
        
         <h1 className='md:text-6xl xs:text-4xl   font-semibold'>WELCOME <br />
         TO 
-           <span className='text-[crimson]'> AFRICA</span>
+           <span className='text-[crimson]'> LAMSKUYY</span>
           </h1>
-        <p className='text-opacity-50 text-xl'>To the continent that prides itself from it's culture,heritage and people </p>
+        <p className='text-opacity-50 text-xl'>Jelajahi Keindahan Nusantara yang Tak Terlupakan </p>
         <div className='btn-container flex justify-center p-2'>
-          <Button type='button' title='Top Destinations' variant='btn_purple shadow' />
+          <Button type='button' title='Tujuan Teratas' variant='btn_purple shadow' />
         </div>
       </div>
 
@@ -24,9 +24,9 @@ const Hero = () => {
       <div className='relative z-1 xl:w-3/5 flexCenter first-line:flex-1'>
       <div className='grid grid-cols-1 md:grid-cols-3  space-4 '>
 
-        <Image src='/9.jpg' alt='frame' width={200} height={400} className='w-250 p-2 rounded-full hover:scale-110 transition-all cursor-pointer'/>          
-        <Image src='/19.jpg' alt='frame' width={200} height={400} className='w-250 p-2 rounded-full hover:scale-110 transition-all cursor-pointer'/>          
-        <Image src='/12.jpg' alt='frame' width={200} height={400} className='w-250 p-2 ml-1 rounded-full hover:scale-110 transition-all cursor-pointer'/>          
+        <Image src='/pahawang1.jpg' alt='frame' width={200} height={400} className='w-250 p-2 rounded-full hover:scale-110 transition-all cursor-pointer'/>          
+        <Image src='/siger.jpg' alt='frame' width={200} height={400} className='w-250 p-2 rounded-full hover:scale-110 transition-all cursor-pointer'/>          
+        <Image src='/Kalinadaresort.jpg' alt='frame' width={200} height={400} className='w-250 p-2 ml-1 rounded-full hover:scale-110 transition-all cursor-pointer'/>          
       </div>
      
       </div>
