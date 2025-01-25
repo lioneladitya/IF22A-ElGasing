ec3ed77cfb7444bb816c76ea62bbd45bef6012fa
Arkan Syah <arkanfadhillahs@egmail.com>
2025-01-26 01:05:39 +0700

    Arkan Fadhillah : update struktur layout dan globals.css untuk styling dan responsivitas yang lebih baik

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,17 +17,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="overflow-x-hidden">
-      <body className="overflow-hidden relative">
+    <html lang="en" className="overflow-x-hidden scroll-smooth">
+      <body className={`${inter.className} relative`}>
+        {/* Dekorasi Lingkaran */}
         <div
           id="home"
-          className="circle-pink h-screen w-screen lg:top-[-40%] lg:left-[-15%;] md:left-[-20%;] sm:top-[-50%] sm:left-[-25%] xs:top-[-50%] xs:right-[40%]"
+          className="circle-pink absolute h-screen w-screen lg:top-[-40%] lg:left-[-15%] md:left-[-20%] sm:top-[-50%] sm:left-[-25%] xs:top-[-50%] xs:right-[40%]"
         />
-        <div className="circle-yellow h-screen w-screen xl:top-[110%] left-[80%] sm:top-[180%]" />
+        <div className="circle-yellow absolute h-screen w-screen xl:top-[110%] left-[80%] sm:top-[180%]" />
+
+        {/* Navbar */}
         <Navbar />
-        <main className="relative bg-gray-950 text-white overflow-hidden">
+
+        {/* Konten Utama */}
+        <main className="relative bg-gray-950 text-white overflow-x-hidden">
           {children}
         </main>
+
+        {/* Footer */}
         <Footer />
       </body>
     </html>
